4ea5160856a7a1790420020e7b7c03e9da40f9fb
_@Nazib_07 <nazib9007.ice.bup@gmail.com>
2026-02-07 21:23:10 +0600

    Add files via upload

---
+++ JavaScript_Math.js
@@ -0,0 +1,18 @@
+let num = 25.6743876324;
+console.log(Math.round(num));//Provide round figure number..
+console.log(Math.ceil(num));//Provide heigest value (number + 1);
+console.log(Math.floor(num));//Provide Lowest value;
+
+// //Squre value...
+console.log(Math.pow(5,2));
+// //Root value..
+console.log(Math.sqrt(81));//Return squre root value..
+console.log(Math.abs(-5));//return positive number all time;
+console.log(Math.min(2, 100, 4, 50, 1, 500, 0));
+console.log(Math.PI);
+
+//Trikonomatric calculation..
+console.log(Math.sin((90 * Math.PI)/180));
+console.log(Math.cos((60 * Math.PI)/180).toFixed(2));
+//Random Number calculation..
+console.log(Math.floor(Math.random().toFixed(2)*10 + 1));
