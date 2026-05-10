814a95438e92d6063aae450a1e042ceaec3f47eb
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-05-10 21:17:33 +0600

    Update constructor.js

--- constructor.js
+++ constructor.js
@@ -1,1 +1,14 @@
 console.log(Math.trunc(4.9))
+console.log(Math.sign(-4));
+console.log(Math.sign(0));
+console.log(Math.sign(4));
+console.log(Math.pow(8,2));
+console.log(Math.sqrt(81));
+console.log(Math.abs(-4.7));
+console.log(Math.round(Math.sin(60 * Math.PI/180)));
+const value = Math.sin(60 * Math.PI/180);
+console.log(Math.round(value));
+
+const value2 = Math.cos(30 * Math.PI/180);
+console.log(Math.floor(value2));
+console.log(Math.round(value2));
